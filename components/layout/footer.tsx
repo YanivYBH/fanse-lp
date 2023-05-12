06c2a425ab77d117cf9bf75aafbca6a9e84adf6d
Yaniv BH <testuser@testusers-MacBook-Pro.local>
2023-05-12 20:02:42 +0300

    new

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,14 @@
 export default function Footer() {
   return (
-    <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
-      <p className="text-gray-500">
-        A free template by{" "}
-        <a
-          className="font-medium text-gray-800 underline transition-colors"
-          href="https://twitter.com/steventey"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Steven Tey
-        </a>
-      </p>
+    <div className="absolute flex w-full flex-col items-center justify-center gap-3 border-t border-gray-200 bg-white py-5 text-center text-xs text-gray-400 sm:flex-row sm:gap-6">
+      <p>@2023 TikTok</p>
+      <div className="flex items-center sm:flex-row flex-col justify-center gap-3 sm:gap-2">
+        <a href="#">TikTok Creator Marketplace Brand Terms of Service</a>
+        <p className="sm:inline-block hidden">|</p>
+        <a href="#">Privacy Policy</a>
+        <p className="sm:inline-block hidden">|</p>
+        <a href="#">Code of Conduct</a>
+      </div>
     </div>
   );
 }
